73546831a91ad85ef8ed5b0d93390390150be880
dangvanthua <thuandang.021102@gmail.com>
2025-02-25 16:53:45 +0700

    implement code to best practice than old code

--- src/main/java/com/viai/ai_docs_reader/repository/BaseRepository.java
+++ src/main/java/com/viai/ai_docs_reader/repository/BaseRepository.java
@@ -1,6 +1,4 @@
 package com.viai.ai_docs_reader.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BaseRepository<T, ID> extends JpaRepository<T, ID> {
+public interface BaseRepository {
 }
--- src/main/java/com/viai/ai_docs_reader/service/base/BaseService.java
+++ src/main/java/com/viai/ai_docs_reader/service/base/BaseService.java
@@ -1,11 +1,4 @@
 package com.viai.ai_docs_reader.service.base;
 
-import java.util.List;
-import java.util.Optional;
-
-public interface BaseService<T, ID> {
-    List<T> getAll();
-    Optional<T> getById(ID id);
-    T save(T entity);
-    void delete(ID id);
+public interface BaseService {
 }
